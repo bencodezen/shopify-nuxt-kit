6479ba7e960fe3770f067357bdc02bf651949b3e
Ben Hong <ben@bencodezen.io>
2021-06-28 13:43:40 -0400

    feature: update product list

---
+++ components/ProductCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    featuredImage() {
+      return this.product.images.edges[0].node
+    },
+    shortDescription() {
+      return this.product.description.substring(0, 60) + '...'
+    },
+  },
+}
+</script>
+
+<template>
+  <div>
+    <NuxtLink :to="`/products/${product.handle}`">
+      <div class="frame">
+        <img
+          class="prodimg"
+          :src="featuredImage.src"
+          :alt="featuredImage.altText"
+        />
+      </div>
+      <h2>{{ product.title }}</h2>
+      <p>{{ shortDescription }}</p>
+    </NuxtLink>
+  </div>
+</template>
