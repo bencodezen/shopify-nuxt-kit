8e63aefe00d04d17101b3f236c6260a206990b2c
Ben Hong <ben@bencodezen.io>
2021-06-28 14:02:01 -0400

    config: add global css file

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/styles/global'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
