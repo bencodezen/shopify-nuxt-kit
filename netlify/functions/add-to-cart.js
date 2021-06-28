a9a1be30e32da5e628c61be748303b39aa8685ff
Ben Hong <ben@bencodezen.io>
2021-06-28 14:00:00 -0400

    docs: add description for add to cart

--- netlify/functions/add-to-cart.js
+++ netlify/functions/add-to-cart.js
@@ -1,18 +1,23 @@
 const { createCart } = require('./utils/createCart')
 const { updateCart } = require('./utils/updateCart')
 
+/**
+ * Expects a POST method
+ * with a body object that contains
+ * the `cartId`
+ */
 exports.handler = async (event) => {
-  const eventBody = JSON.parse(event.body)
+  const { cartId } = JSON.parse(event.body)
 
-  if (eventBody.cartId) {
-    const updateCartResponse = await updateCart(eventBody)
+  if (cartId) {
+    const updateCartResponse = await updateCart(cartId)
 
     return {
       statusCode: 200,
       body: JSON.stringify(updateCartResponse),
     }
   } else {
-    const createCartResponse = await createCart(eventBody)
+    const createCartResponse = await createCart(cartId)
 
     return {
       statusCode: 200,
