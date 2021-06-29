d50d295e9f319dfc29d44fb2c5553d88f7715962
Ben Hong <ben@bencodezen.io>
2021-06-29 08:13:17 -0400

    refactor: use try catch block to standardize endpoint design

--- netlify/functions/get-product.js
+++ netlify/functions/get-product.js
@@ -18,59 +18,63 @@ const { postToShopify } = require('./utils/postToShopify')
 exports.handler = async (event) => {
   const { itemHandle } = JSON.parse(event.body)
 
-  console.log('--------------------------------')
-  console.log('Retrieving product details...')
-  console.log('--------------------------------')
-  const shopifyResponse = await postToShopify({
-    query: `
-      query getProduct($handle: String!) {
-        productByHandle(handle: $handle) {
-          id
-          handle
-          description
-          title
-          totalInventory
-          variants(first: 5) {
-            edges {
-              node {
-                id
-                title
-                quantityAvailable
-                priceV2 {
-                  amount
-                  currencyCode
+  try {
+    console.log('--------------------------------')
+    console.log('Retrieving product details...')
+    console.log('--------------------------------')
+    const shopifyResponse = await postToShopify({
+      query: `
+        query getProduct($handle: String!) {
+          productByHandle(handle: $handle) {
+            id
+            handle
+            description
+            title
+            totalInventory
+            variants(first: 5) {
+              edges {
+                node {
+                  id
+                  title
+                  quantityAvailable
+                  priceV2 {
+                    amount
+                    currencyCode
+                  }
+                }
+              }
+            }
+            priceRange {
+              maxVariantPrice {
+                amount
+                currencyCode
+              }
+              minVariantPrice {
+                amount
+                currencyCode
+              }
+            }
+            images(first: 1) {
+              edges {
+                node {
+                  src
+                  altText
                 }
               }
             }
           }
-          priceRange {
-            maxVariantPrice {
-              amount
-              currencyCode
-            }
-            minVariantPrice {
-              amount
-              currencyCode
-            }
-          }
-          images(first: 1) {
-            edges {
-              node {
-                src
-                altText
-              }
-            }
-          }
         }
-      }
-    `,
-    variables: {
-      handle: itemHandle,
-    },
-  })
+      `,
+      variables: {
+        handle: itemHandle,
+      },
+    })
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(shopifyResponse),
+    return {
+      statusCode: 200,
+      body: JSON.stringify(shopifyResponse),
+    }
+  } catch (error) {
+    console.log(error)
   }
 }
