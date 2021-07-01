acbfe957fbaadf6ce9ff854f08da33c4debe3e39
Ben Hong <ben@bencodezen.io>
2021-06-30 23:33:42 -0400

    refactor: delete unused code

--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,6 @@ export default {
       productList: productListResponse.products.edges,
     }
   },
-  data: () => ({
-    shoppingCart: 0,
-    cart: {},
-  }),
   mounted() {
     // Get local cart
     const localCart = window.localStorage.getItem('shopifyNuxtCart')
@@ -19,29 +15,13 @@ export default {
       this.$store.dispatch('cart/updateBase', JSON.parse(localCart))
     }
   },
-  methods: {
-    async addToCart() {
-      const cartResponse = await fetch('/api/add-to-cart', {
-        method: 'POST',
-        body: JSON.stringify({
-          cartId: this.cartId,
-          itemId:
-            'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zOTc0NDA5ODA0MjA2Mg==',
-          quantity: 1,
-        }),
-      })
-      const cartData = await cartResponse.json()
-
-      this.$store.dispatch('cart/updateCartId', cartData.id)
-    },
-  },
 }
 </script>
 
 <template>
   <main>
     <main-nav></main-nav>
-    <HomeHero :cart="shoppingCart" />
+    <HomeHero />
     <div>
       <ul class="products">
         <li
