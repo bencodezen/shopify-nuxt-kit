470e48830bbd19271260d255b75e7884f10ad341
Ben Hong <ben@bencodezen.io>
2021-07-08 01:17:47 -0400

    refactor: create cart table component

---
+++ components/CartTable.vue
@@ -0,0 +1,90 @@
+<script>
+import { mapGetters } from 'vuex'
+import { formatCurrency } from '../utils/currency'
+
+export default {
+  computed: {
+    ...mapGetters({
+      cartItems: 'cart/items',
+    }),
+  },
+  methods: {
+    formatCurrency,
+    itemTotal(price, quantity) {
+      const totalPrice = Number(price.amount) * Number(quantity)
+
+      return this.formatCurrency(totalPrice, price.currencyCode)
+    },
+    async removeItem(lineId) {
+      const shopifyResponse = await this.$http.$post('/api/remove-from-cart', {
+        cartId: this.cartId,
+        lineId,
+      })
+
+      this.$store.dispatch('cart/updateBase', shopifyResponse)
+    },
+  },
+}
+</script>
+
+<template>
+  <table class="cart-table">
+    <thead>
+      <th class="cart-table-heading">Item</th>
+      <th class="cart-table-heading">Price</th>
+      <th class="cart-table-heading">Quantity</th>
+      <th class="cart-table-heading">Total</th>
+      <th class="cart-table-heading">Actions</th>
+    </thead>
+    <tbody>
+      <tr
+        v-for="{ node: item } in cartItems"
+        :key="item.id"
+        class="cart-table-row"
+      >
+        <td class="cart-table-cell">
+          <nuxt-link :to="`/products/${item.merchandise.product.handle}`">
+            {{ item.merchandise.product.title }} ({{ item.merchandise.title }})
+          </nuxt-link>
+        </td>
+        <td class="cart-table-cell">
+          {{
+            formatCurrency(
+              item.merchandise.priceV2.amount,
+              item.merchandise.priceV2.currencyCode
+            )
+          }}
+        </td>
+        <td class="cart-table-cell">{{ item.quantity }}</td>
+        <td class="cart-table-cell">
+          {{ itemTotal(item.merchandise.priceV2, item.quantity) }}
+        </td>
+        <td class="cart-table-cell">
+          <button @click="removeItem(item.id)">Remove Item</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style lang="scss">
+.cart-table {
+  width: 100%;
+  margin-top: 20px;
+  margin-bottom: 30px;
+}
+
+.cart-table-cell {
+  padding: 8px 0;
+  border-bottom: 1px solid #ccc;
+}
+
+.cart-table-heading {
+  padding: 10px 0;
+  border-bottom: 1px solid #ccc;
+}
+
+.cart-table-row {
+  text-align: center;
+}
+</style>
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,6 +1,5 @@
 <script>
 import { mapGetters } from 'vuex'
-import { formatCurrency } from '../utils/currency'
 
 export default {
   head: () => ({
@@ -31,22 +30,6 @@ export default {
       }
     }
   },
-  methods: {
-    formatCurrency,
-    itemTotal(price, quantity) {
-      const totalPrice = Number(price.amount) * Number(quantity)
-
-      return this.formatCurrency(totalPrice, price.currencyCode)
-    },
-    async removeItem(lineId) {
-      const shopifyResponse = await this.$http.$post('/api/remove-from-cart', {
-        cartId: this.cartId,
-        lineId,
-      })
-
-      this.$store.dispatch('cart/updateBase', shopifyResponse)
-    },
-  },
 }
 </script>
 
@@ -55,45 +38,7 @@ export default {
     <article class="cart-content">
       <h1>Your Cart</h1>
       <div v-if="cartItems.length > 0">
-        <table class="cart-table">
-          <thead>
-            <th class="cart-heading">Item</th>
-            <th class="cart-heading">Price</th>
-            <th class="cart-heading">Quantity</th>
-            <th class="cart-heading">Total</th>
-            <th class="cart-heading">Actions</th>
-          </thead>
-          <tbody>
-            <tr
-              v-for="{ node: item } in cartItems"
-              :key="item.id"
-              class="cart-row"
-            >
-              <td class="cart-row-item">
-                <nuxt-link :to="`/products/${item.merchandise.product.handle}`">
-                  {{ item.merchandise.product.title }} ({{
-                    item.merchandise.title
-                  }})
-                </nuxt-link>
-              </td>
-              <td class="cart-row-item">
-                {{
-                  formatCurrency(
-                    item.merchandise.priceV2.amount,
-                    item.merchandise.priceV2.currencyCode
-                  )
-                }}
-              </td>
-              <td class="cart-row-item">{{ item.quantity }}</td>
-              <td class="cart-row-item">
-                {{ itemTotal(item.merchandise.priceV2, item.quantity) }}
-              </td>
-              <td class="cart-row-item">
-                <button @click="removeItem(item.id)">Remove Item</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <cart-table />
         <cart-total :subtotal="subtotal" :tax="tax" :total="total"></cart-total>
       </div>
       <section v-else>
@@ -124,24 +69,5 @@ export default {
 .cart-wrapper {
   width: 80vw;
   margin: 0 auto;
-}
-
-.cart-table {
-  width: 100%;
-  margin-top: 20px;
-  margin-bottom: 30px;
-}
-
-.cart-item-row {
-  text-align: center;
-}
-
-.cart-heading {
-  padding: 10px 0;
-}
-
-.cart-row-item,
-.cart-heading {
-  border-bottom: 1px solid #ccc;
 }
 </style>
