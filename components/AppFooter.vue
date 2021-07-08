510316254831eb58f8cfbb08e79d860dfee5c50c
Ben Hong <ben@bencodezen.io>
2021-07-08 00:33:55 -0400

    feature: make footer responsive

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <section class="callout">
+    <section class="testimonial">
       <h2>
         "The interplay of flavors between the cheese, meats and fruits is an
         absolute delight."
@@ -8,7 +8,7 @@
       <p>Paul Hotcakes</p>
     </section>
     <app-footer-links />
-    <div class="sarahstuff">
+    <div class="project-credit">
       <p>
         This project is
         <a href="https://github.com/bencodezen/shopify-nuxt">
@@ -24,19 +24,14 @@
 </template>
 
 <script>
-import AppFooterLinks from '~/components/AppFooterLinks.vue'
-export default {
-  components: {
-    AppFooterLinks,
-  },
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
-.callout {
+.testimonial {
   width: 100%;
   height: 280px;
-  background: url('/images/callout.jpg') center center no-repeat;
+  background: url('/images/testimonial-bg.jpg') center center no-repeat;
   background-size: cover;
   display: flex;
   justify-content: center;
@@ -48,17 +43,18 @@ export default {
     text-align: center;
   }
 }
-.sarahstuff {
-  background: black;
+.project-credit {
   width: 100%;
   padding: 10px 30px;
+  background: black;
   color: white;
   text-align: center;
-}
-a,
-a:visited,
-a:active {
-  color: #c4c3ec;
-  font-weight: bold;
+
+  a,
+  a:visited,
+  a:active {
+    color: #22aaff;
+    font-weight: bold;
+  }
 }
 </style>
--- components/AppFooterLinks.vue
+++ components/AppFooterLinks.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="app-footer-links">
     <ul>
       <li>About</li>
       <li>Company</li>
@@ -13,10 +13,14 @@
       <li>Instagram</li>
       <li>LinkedIn</li>
     </ul>
-    <ul>
+    <div class="newsletter">
       <h2 class="newsletter-title">Sign up for our newsletter:</h2>
-      <input type="email" placeholder="Enter your email" />
-    </ul>
+      <input
+        class="newsletter-input"
+        type="email"
+        placeholder="Enter your email"
+      />
+    </div>
   </section>
 </template>
 
@@ -25,27 +29,42 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-section {
+.app-footer-links {
   width: 80%;
-  margin-left: 10%;
   padding: 40px 0;
+  margin-left: 10%;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  grid-row-gap: 30px;
+
+  @include breakpoint($deviceLg) {
+    grid-template-columns: 1fr 1fr 2fr;
+    grid-template-rows: 1fr;
+    grid-row-gap: 0;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
 }
-ul {
-  list-style: none;
-  padding-left: 0;
-}
-input {
+
+.newsletter {
   width: 100%;
-}
-@media screen and (min-width: 850px) {
-  section {
-    padding: 40px !important;
+  grid-column: 1 / span 2;
+
+  @include breakpoint($deviceLg) {
+    grid-column: 3;
   }
 }
 
 .newsletter-title {
-  margin-bottom: 0.5rem;
+  margin-bottom: 1rem;
+}
+
+.newsletter-input {
+  width: 100%;
+  padding: 10px;
 }
 </style>
